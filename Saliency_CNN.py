e23259c451f4bebe782b47bbce1c39c1caf61b30
Mikkeline Havgaard <Mikkeline.havgaard@gmail.com>
2024-11-21 09:59:50 +0100

    Added upsampling

--- Saliency_CNN.py
+++ Saliency_CNN.py
@@ -1,10 +1,13 @@
-import tensorflow as tf 
 from keras import models, layers
 from sklearn.model_selection import train_test_split  
+#from tensorflow.keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt 
 import os, glob
 import numpy as np   
 import cv2 
+
+batchsize = 4
+epoch = 5
 
 # Define the stream model:
 def build_stream(input_shape,stream_name):
@@ -26,9 +29,14 @@ def build_stream(input_shape,stream_name):
     model.add(layers.Conv2D(256, (3, 3), dilation_rate=4, activation='relu',strides=1,padding='same'))   
  
     model.add(layers.Dropout(0.5))  
-    model.add(layers.Conv2D(1, (1, 1), activation='sigmoid')) 
 
+    # Upsampling
+    model.add(layers.Conv2DTranspose(128, (4, 4), activation='relu', strides=2, padding='same'))  # 50 -> 100
+    model.add(layers.Conv2DTranspose(64, (4, 4), activation='relu', strides=2, padding='same'))   # 100 -> 200
+    model.add(layers.Conv2DTranspose(32, (13, 13), activation='relu', strides=1, padding='valid'))  
+    model.add(layers.Conv2DTranspose(3, (13, 13), activation='sigmoid', strides=1, padding='valid'))  
     model.summary()
+
     return model 
 
 # Define the fusion model:
@@ -60,24 +68,35 @@ def build_fusion_model(rgb_shape, depth_shape):
     return model
 
 # Paths to the dataset
-rgb_dir = sorted(glob.glob(os.path.join("dataset","RGB_left", "**")))
-depth_dir = sorted(glob.glob(os.path.join("dataset","depth", "**")))
-gt_dir =sorted(glob.glob(os.path.join("dataset","GT-1985", "**")) )
+rgb_train_dir = sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Testing\RGB\*.png")))
+rgb_valid_dir = sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Validation\RGB\*.png")))
+depth_train_dir = sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Testing\HHA\*.png")))
+depth_valid_dir = sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Validation\HHA\*.png")))
+gt_train_dir =sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Testing\GT\*.png")) )
+gt_valid_dir =sorted(glob.glob(os.path.join(r"C:\Users\mikke\Desktop\Final_dataset\Final_dataset\Validation\GT\*.png")) )
 
 # Define the image sizes
 img_size = (224, 224)
-gt_size = (50,50)
+gt_size = (224,224)
 
 # Loading and resize the images 
+#print("Resizing images...")
+#rgb_images = np.array([cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0 for file in rgb_dir]) 
+#depth_images = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in depth_dir])  # Reducing the number of channels to 1 
+#gt_images = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), gt_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in gt_dir]) # Reducing the number of channels to 1 
+#print("Images resized.")  
+
 print("Resizing images...")
-rgb_images = np.array([cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0 for file in rgb_dir]) 
-depth_images = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in depth_dir])  # Reducing the number of channels to 1 
-gt_images = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), gt_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in gt_dir]) # Reducing the number of channels to 1 
+RGB_train = np.array([cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0 for file in rgb_train_dir]) 
+RGB_valid = np.array([cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0 for file in rgb_valid_dir]) 
+depth_train = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in depth_train_dir])
+depth_valid = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), img_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in depth_valid_dir])
+GT_train = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), gt_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in gt_train_dir])
+GT_valid = np.array([cv2.cvtColor(cv2.resize(cv2.imread(file), gt_size).astype('float32')/255.0, cv2.COLOR_RGB2GRAY) for file in gt_valid_dir])
 print("Images resized.")  
 
-
 # Split the dataset into training and validation sets
-RGB_train, RGB_valid, depth_train, depth_valid, GT_train, GT_valid=train_test_split(rgb_images, depth_images, gt_images, test_size=0.2,shuffle=True)  
+#RGB_train, RGB_valid, depth_train, depth_valid, GT_train, GT_valid=train_test_split(rgb_images, depth_images, gt_images, test_size=0.2,shuffle=True)  
 
 # Build the fusion model
 model = build_fusion_model((img_size[0], img_size[1], 3), (img_size[0], img_size[1], 1))
@@ -86,8 +105,10 @@ model = build_fusion_model((img_size[0], img_size[1], 3), (img_size[0], img_size
 history = model.fit(
     [RGB_train, depth_train],  # Training data
     GT_train, # Ground truth labels
-    epochs=10,
-    batch_size=16,   
+    epochs=epoch,
+    batch_size=batchsize, 
+    validation_freq=1,  
+    shuffle=True,
     validation_data=([RGB_valid, depth_valid], GT_valid)  
 )  
 
@@ -95,7 +116,7 @@ history = model.fit(
 model.save("Saliency_model.keras")
 
 # Evaluate the model 
-loss, accuracy = model.evaluate([RGB_valid, depth_valid], GT_valid) 
+loss, accuracy = model.evaluate([RGB_valid, depth_valid], GT_valid,batch_size=batchsize,training=False) 
 print("Loss: ", loss) 
 print("Accuracy: ", accuracy)  
 
@@ -106,6 +127,10 @@ plt.figure(figsize=(14, 5))
 plt.subplot(1, 2, 1)
 plt.plot(history.history['accuracy'], label='Training Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+
+plt.ylim(0, 1)
+plt.xlim(0,epoch-1)
+
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.title('Training and Validation Accuracy')
@@ -115,6 +140,9 @@ plt.legend()
 plt.subplot(1, 2, 2)
 plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
+
+plt.ylim(0, 1)
+plt.xlim(0,epoch-1)
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.title('Training and Validation Loss')
