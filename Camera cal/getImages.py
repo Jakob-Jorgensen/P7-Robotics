6e667cd3811218db6f737ea75d8575f73d0e413f
SimonWhiteSimon <89812516+SimonWhiteSimon@users.noreply.github.com>
2024-12-10 02:25:33 +0100

    Camera Calibration

---
+++ Camera cal/getImages.py
@@ -0,0 +1,25 @@
+import cv2
+
+cap = cv2.VideoCapture(2)
+
+num = 0
+
+while cap.isOpened():
+
+    succes, img = cap.read()
+
+    k = cv2.waitKey(5)
+
+    if k == 27:
+        break
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite(r'C:\Users\simao\Documents\aau\1ST SEMESTER\project\camera cal\CameraCalibration\images\images2\img' + str(num) + '.png', img)
+        print("image saved!")
+        num += 1
+
+    cv2.imshow('Img',img)
+
+# Release and destroy all windows before termination
+cap.release()
+
+cv2.destroyAllWindows()
